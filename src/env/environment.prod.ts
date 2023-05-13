1f4aa164c0a2c215ac5f079cb886b110f8d0dd3c
daniel <davillera77@gmail.com>
2023-05-13 15:15:31 -0500

    envCheck

--- src/env/environment.prod.ts
+++ src/env/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyCJYavgpmwCAV6jSG2SEWo094vmm8gjCzA",
+    authDomain: "softpymes-825f5.firebaseapp.com",
+    projectId: "softpymes-825f5",
+    storageBucket: "softpymes-825f5.appspot.com",
+    messagingSenderId: "173148407105",
+    appId: "1:173148407105:web:4fe456a363965fc58e4dc0"
+  }
 };
